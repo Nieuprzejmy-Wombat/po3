89ca7ade10d77646c99bec40a7d7ffead0dd8541
wombat <u58_makcza_waw@technischools.com>
2024-02-21 14:03:33 +0100

    wrote unit tests for persistence layer

--- src/persistence.py
+++ src/persistence.py
@@ -1,4 +1,4 @@
-from datatypes import internal_user
+from .datatypes import internal_user
 
 
 class Repository:
@@ -16,8 +16,8 @@ class Repository:
 
     def modify_user(self, user: tuple[int, str | None, str | None, int | None, str | None]) -> None:
         old_user = self.users[user.id]
-        self.users[user.id] = (val if val is not None else old_user[i]
-                               for i, val in enumerate(user))
+        self.users[user.id] = internal_user(*(val if val is not None else old_user[i]
+                                            for i, val in enumerate(user)))
 
     def delete_user(self, id: int) -> None:
         del self.users[id]
--- test/unit/test_persistence.py
+++ test/unit/test_persistence.py
@@ -1,6 +1,6 @@
 from pytest import fixture
-from src.datatypes import internal_user
-from src.persistence import Repository
+from ...src.datatypes import internal_user
+from ...src.persistence import Repository
 
 
 @fixture
@@ -22,3 +22,28 @@ def test_get_users_returns_all_users(user1, user2, repository) -> None:
     repository.create_user(user1)
     repository.create_user(user2)
     assert repository.get_users() == {user1.id: user1, user2.id: user2}
+
+
+def test_get_user_returns_correct_user(user1, user2, repository) -> None:
+    repository.create_user(user1)
+    repository.create_user(user2)
+    assert repository.get_user(user1.id) == user1
+
+
+def test_create_user_creates_user(user1, repository) -> None:
+    repository.create_user(user1)
+    assert repository.get_user(user1.id)
+
+
+def test_modify_user_changes_user(user1, repository) -> None:
+    repository.create_user(user1)
+    new_user = internal_user(user1.id, user1.first_name, user1.last_name, user1.age + 1, user1.group)
+    repository.modify_user(new_user)
+    assert repository.get_user(user1.id) == new_user
+
+
+def test_delete_user_deletes_existing_user(user1, repository) -> None:
+    repository.create_user(user1)
+    repository.delete_user(user1.id)
+    assert repository.get_users() == {}
+
